8ea0fc5b22f55014b6b24ec0cf9c29d46f289d2c
Antoine <antoine.saintlouis@gmail.com>
2020-10-18 12:21:54 +0200

    usles

--- log.js
+++
@@ -1,6 +0,0 @@
-function Log(){
-    const img = document.getElementById('img');
-    fetch ('https://api.thecatapi.com/v1/images/search')
-    .then (res => res.json())
-    .then( data => img.src = data[0].url)
-}
